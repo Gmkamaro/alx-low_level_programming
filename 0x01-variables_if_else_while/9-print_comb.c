df4f7bea5c9c7270ef65d0071dfc51bcf2d8b3b7
FathiHamdi <hamdifathi60@gmail.com>
2022-12-09 14:28:22 -0800

    y

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Use `putchar` function to print the alphabet in lowercase.
+ * Return: 0
+ */
+int main(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+		putchar(',');
+		putchar(' ');
+	}
+
+	return (0);
+}
